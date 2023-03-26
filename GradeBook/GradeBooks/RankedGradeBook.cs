0d6a5618e04c5cd4ec6d02cbf12d2bdcef13c10a
s0bczynski <sobczynski.mateusz@o2.pl>
2023-03-26 20:34:04 +0200

    Overrided CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -45,6 +45,18 @@ namespace GradeBook.GradeBooks
 			}
 			return base.GetLetterGrade(averageGrade);
 		}
+
+		public override void CalculateStatistics()
+		{
+			if (Students.Count < 5)
+			{
+				Console.WriteLine("Ranked grading requires at least 5 students.");
+			}
+			else
+			{
+				base.CalculateStatistics();
+			}
+		}
 		public override void CalculateStudentStatistics(string name)
 		{
 			if(Students.Count < 5)
